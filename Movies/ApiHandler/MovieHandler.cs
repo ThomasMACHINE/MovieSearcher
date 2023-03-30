35a2ea588da1d336cd7daec4149dc7f5a9f7f6b3
sabanaghale <ThomasLien@outlook.com>
2023-03-30 11:02:55 +0100

    Added ApiHandler

---
+++ Movies/ApiHandler/MovieHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Movies.Models;
+using System.Text.Json;
+
+namespace Movies.ApiHandler
+{
+    public static class MovieHandler
+    {
+        private static HttpClient client = new HttpClient();
+        private static string baseLink = "http://www.omdbapi.com/?apikey=4839f77";
+
+        public static async Task<Movie> CreateMovie(string Title)
+        {
+            Movie movie = null;
+            // Format Search Link
+            string searchLink = baseLink + "&t=" + Title;
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(searchLink);
+                response.EnsureSuccessStatusCode();
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                movie = JsonSerializer.Deserialize<Movie>(responseContent);
+            }
+            catch (Exception e)
+            {
+                // Debug
+                return new Movie { Title = e.ToString() };
+            }
+            return movie;
+        }
+    }
+}
